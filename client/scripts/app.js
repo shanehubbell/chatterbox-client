ed6ec266d964682a5880f8c948b9b93b62eb1754
shanehubbell <shane.hubbell@gmail.com>
2016-05-02 19:29:51 -0700

    Mostly working version of Chatterbox.  Committing before we refactor.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,7 @@
 // YOUR CODE HERE:
 
 var url = 'https://api.parse.com/1/classes/messages';
+var roomSelection;
 
 // http://shebang.brandonmintern.com/foolproof-html-escaping-in-javascript/
 var escapeHtml = function (str) {
@@ -50,10 +51,11 @@ var app = {
   },
 
   clearMessages: function () {
+    $('#chats').empty();
   },
 
   addMessage: function(message) {
-
+    postMessage(message);
     //on submit, we need to grab the username, text, & roomname
     //wrap those in an object like below
     //then send those.
@@ -80,6 +82,7 @@ $( document ).ready(function() {
   });
 
   $('.submitButton').click(function() {
+    app.clearMessages();
     var userInput = $('.userInput').val();
     var username = location.search.slice(10); // refactor this later to make it better
     var roomname = '';
@@ -93,20 +96,32 @@ $( document ).ready(function() {
 
   });
   
-  var postMessage = function () {
+  var postMessage = function (filterName) {
     app.fetch(url, function (data) {
+      $('#chats').empty();
       for (var i = 0; i < data.results.length; i++) {
-
         var username = escapeHtml(data.results[i].username);
         var text = escapeHtml(data.results[i].text);
         var roomname = escapeHtml(data.results[i].roomname);
-        $('#chats').append('<div>Username: ' + username + ' Message: ' + text + ' Roomname: ' + roomname + '</div>');
+        if (filterName !== undefined) {
+          if (filterName === roomname) {
+            $('#chats').append('<div>Username: ' + username + ' Message: ' + text + ' Roomname: ' + roomname + '</div>');
+          }
+        } else {
+          $('#chats').append('<div>Username: ' + username + ' Message: ' + text + ' Roomname: ' + roomname + '</div>');
+        }
       }
       updateRooms();
     });
   };
 
-  var updateRooms = function () {
+  $('#roomNames').change(function () {
+    roomSelection = $('#roomNames option:selected').text();
+    postMessage(roomSelection);
+  });
+
+  var updateRooms = function (room) {
+
     app.fetch(url, function (data) {
       var roomNames = [];
       for (var i = 0; i < data.results.length; i++) {
@@ -114,8 +129,13 @@ $( document ).ready(function() {
         roomNames.push(roomname);
       }
       roomNames = _.uniq(roomNames);
+      $('#roomNames').empty();
       _.each(roomNames, function(value, index, collection) {
-        $('#roomNames').append(`<option>${value}</option>`);
+        if (value === roomSelection) {
+          $('#roomNames').append(`<option selected="selected">${value}</option>`);
+        } else {
+          $('#roomNames').append(`<option>${value}</option>`);
+        }
       });
     });
   };
